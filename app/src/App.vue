f1dabd3e116be30e58b2641251d157e0ccd61b7e
Elarosi <cabry95@protonmail,com>
2022-06-22 12:02:48 +0200

    menu component added

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1 class="headline">Menu</h1>
+    <Menu />
+  </div>
+</template>
+
+<script>
+import Menu from "./components/Menu.vue";
+import './styles/app.css'
+export default {
+  name: "App",
+  components: {
+    Menu,
+  },
+  mounted() {},
+};
+</script>
---
+++ app/src/components/Menu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="card" v-for="item in list" v-bind:key="item.id">
+      <li>{{ item.productName }}</li>
+      <li>{{ item.image }}</li>
+      <li>{{ `Category: ${item.category}` }}</li>
+      <li>{{ `Country: ${item.from}` }}</li>
+      <li>{{ `Price: ${item.price} €` }}</li>
+      <li>{{ `Description: ${item.description}` }}</li>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import '../styles/menu.css';
+
+Vue.use(VueAxios, axios);
+
+export default {
+  name: 'Menu',
+  data() {
+    return {
+      list: undefined,
+      card: {},
+    };
+  },
+  async mounted() {
+    Vue.axios.get('http://127.0.0.1:9001/dishes').then((res) => {
+      this.list = res.data.data;
+      console.warn(res.data.data);
+    });
+  },
+  methods: {
+    async addItem() {
+
+    },
+  }
+};
+</script>
